fee82a1c02dc55005a7d82ac4a6e436ee53bf09d
Vicky Lau <vlau.nyc@gmail.com>
2018-01-22 16:53:49 -0500

    added to formulas contoller and views.

--- app/controllers/formulas_controller.rb
+++ app/controllers/formulas_controller.rb
@@ -1,6 +1,44 @@
 class FormulasController < ApplicationController
+  before_action :find_formula, only: [:show, :edit, :update, :destroy]
 
   def index
+    @formulas = Formula.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @formula = Formula.new
+  end
+
+  def create
+    @formula = Formula.new(formula_params)
+
+    if @formula.save
+      redirect_to @formula, notice: "Yay! Successfully created new formula!"
+    else
+      redner 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def formula_params
+    params.require(:formula).permit(:user_id, :title, :description, :direction)
+  end
+
+  def find_formula
+    @formula = Formula.find(params[:id])
   end
 
 end
