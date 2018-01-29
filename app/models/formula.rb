a10031fb5f0cde629bbd96ff6b433b42c00a0f06
Vicky Lau <vlau.nyc@gmail.com>
2018-01-29 17:48:53 -0500

    downcase skinconcerns, validates uniq

--- app/models/formula.rb
+++ app/models/formula.rb
@@ -2,20 +2,16 @@ class Formula < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :ingredients
-  # accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }
   accepts_nested_attributes_for :ingredients, :reject_if => :all_blank, :allow_destroy => true
 
-  # has_many :skinconcern_formulas
-  # has_many :skinconcerns, through: :skinconcern_formulas
   has_and_belongs_to_many :skinconcerns, -> { uniq }
   accepts_nested_attributes_for :skinconcerns, :reject_if => :all_blank
-
+ 
   validates :title, :description, :category, :ingredients, :direction, presence: true
 
   #paperclip, to add image file
   has_attached_file :image, styles: { :medium => "200x200#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
 
   before_save :destroy_image?
 
--- app/models/skinconcern.rb
+++ app/models/skinconcern.rb
@@ -1,8 +1,6 @@
 class Skinconcern < ActiveRecord::Base
-  # has_many :skinconcern_formulas
-  # has_many :formulas, through: :skinconcern_formulas
   has_and_belongs_to_many :formulas
-
+  validates :name, uniqueness: { case_sensitive: false }
   before_save :downcase_fields
 
   def downcase_fields
