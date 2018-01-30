6a75268fc122127e691ca82d0f10335d798499ca
Vicky Lau <vlau.nyc@gmail.com>
2018-01-29 19:39:10 -0500

    revised category instead of Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,13 @@
 class CategoriesController < ApplicationController
 
   def index
-    # @categories = Category.all.order("created_at DESC")
      @face_categories = Category.all.where(:part => 'face')
      @hair_categories = Category.all.where(:part => 'hair')
      @body_categories = Category.all.where(:part => 'body')
   end
 
   def show
-    @Category = Category.find(params[:id])
+    @category = Category.find(params[:id])
   end
 
 end
