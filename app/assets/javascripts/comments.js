ee6096040188c304ab4b35c9c63e494d54a54307
Vicky Lau <vlau.nyc@gmail.com>
2018-04-14 17:07:35 -0400

    corrected html rendering of comments in formula show page. Moved comment js out for formula file.

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,43 @@
+$(function(){
+   $("#new_comment").on("submit", function(e){
+     $.ajax({
+       type: "POST",
+       url: this.action,
+       data: $(this).serialize(), // either JSON or querystring serializing
+       success: function(response){
+        //update the DOM
+        var comment = new Comment(response);
+        comment.renderComments();
+        $(".commentBox").val("");
+       }
+     });
+
+     e.preventDefault();
+   })
+ });
+
+function Comment(data) {
+  this.id = data.id;
+  this.content = data.content;
+  this.user = data.user;
+}
+
+Comment.prototype.renderComments = function() {
+  var html = "" ;
+  html += 
+  "<br>" +
+  "<div class=\'card' id=\'comment-" + this.id + "'>" +
+    "<div class=\'card-body'>" +
+      "<h6 class=\'card-subtitle mb-2 text-muted'>Posted by: "  + 
+        "<a href='/users/" + this.user.id + "'>" + this.user.username + "</a>" + 
+      "</h6>" + 
+      "<p class=\'card-text'>" + this.content + "</p>"
+    "</div>"
+  "</div>";
+
+  $("#submitted-comments").append(html);
+}
+
+
+
+   
