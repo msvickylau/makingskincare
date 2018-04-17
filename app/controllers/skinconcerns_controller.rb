a00b79787693799f26b09ed81c1d4dff18c16d1f
Vicky Lau <vlau.nyc@gmail.com>
2018-04-17 17:35:54 -0400

    created custom serializer for formula_list

--- app/controllers/skinconcerns_controller.rb
+++ app/controllers/skinconcerns_controller.rb
@@ -6,5 +6,25 @@ class SkinconcernsController < ApplicationController
   def show
     @skinconcern = Skinconcern.find(params[:id])
     @formulas = @skinconcern.formulas.order("created_at DESC")
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json:  @skinconcern }
+    end
   end
+
+  # def next_skinconcern 
+      #get '/skinconcerns/:id/next', to: 'skinconcerns#next_skinconcern'
+  #   @skinconcern = Skinconcern.find(params[:id])
+  #   @next_skinconcern = @skinconcern.next
+  #   render json: @next_skinconcern
+  # end
+
+  # def previous_skinconcern 
+      #get '/skinconcerns/:id/previous', to: 'skinconcerns#previous_skinconcern'
+  #   @skinconcern = Skinconcern.find(params[:id])
+  #   @previous_skinconcern = @skinconcern.previous
+  #   render json: @previous_skinconcern
+  # end
+
 end
