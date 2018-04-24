34b9d39f526258b47e13db2f74755830d4ead77d
Vicky Lau <vlau.nyc@gmail.com>
2018-04-24 13:03:39 -0400

    moved new date var inside function so ingredients will save with unique time identifier.

--- app/assets/javascripts/formulas.js
+++ app/assets/javascripts/formulas.js
@@ -12,35 +12,36 @@ $(function() {
 
 //Formula Form
 $(document).ready(function(){
-  //create date object
-  var date = new Date();
-
-  //get number of milliseconds since midnight Jan 1, 1970  
-  //and use it for address key 
-  var mSec = date.getTime();
-
-  //Replace 0 with milliseconds 
-  idAttributeIngredient = 
-   "formula_ingredients_attributes_0_name".replace("0", mSec);
-  nameAttributeIngredient = 
-    "formula[ingredients_attributes][0][name]".replace("0", mSec);
-
+ 
   $('#addNewIngredient').click(function(){
-  $('div#ingredientsSet').append(
-        "<div class='ingredientsForm input-group mb-3'>" +
-          "<input class='form-control' type='text' name='"+ nameAttributeIngredient+"' id='"+idAttributeIngredient+"'>" +
-          "<div class='input-group-append'>" +
-            "<span class='input-group-text'>" +
-              "<button type='button' class='removeIngredient close'> ×</button>" +
-            "</span>" +
-          "</div>" +
-        "</div>"
+
+    //create date object
+    var date = new Date();
+
+    //get number of milliseconds since midnight Jan 1, 1970  
+    //and use it for address key 
+    var mSec = date.getTime();
+
+    //Replace 0 with milliseconds 
+    idAttributeIngredient = "formula_ingredients_attributes_0_name".replace("0", mSec);
+    nameAttributeIngredient = "formula[ingredients_attributes][0][name]".replace("0", mSec);
+
+
+    $('div#ingredientsSet').append(
+      "<div class='ingredientsForm input-group mb-3'>" +
+        "<input class='form-control' type='text' name='"+ nameAttributeIngredient+"' id='"+idAttributeIngredient+"'>" +
+        "<div class='input-group-append'>" +
+          "<span class='input-group-text'>" +
+            "<button type='button' class='removeIngredient close'> ×</button>" +
+          "</span>" +
+        "</div>" +
+      "</div>"
     );
   });
     
-$("div#ingredientsSet").on('click', '.removeIngredient', function(){
-  //closest goes up thru the DOM, looking for the first ancestor with the class ingredientsForm
-  $(this).closest('.ingredientsForm').remove(); 
-  console.log(this)
-});
+  $("div#ingredientsSet").on('click', '.removeIngredient', function(){
+    //closest goes up thru the DOM, looking for the first ancestor with the class ingredientsForm
+    $(this).closest('.ingredientsForm').remove(); 
+    console.log(this)
+  });
 });
